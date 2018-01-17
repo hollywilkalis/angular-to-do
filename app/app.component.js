63c0e4d5b95da6e22b0f8d75f688b83215d0fbf4
Guest User <Guest@Epicodus-4D.local>
2018-01-16 16:11:29 -0800

    Add task priority

--- app/app.component.js
+++ app/app.component.js
@@ -15,23 +15,46 @@ var AppComponent = (function () {
         this.day = this.currentTime.getDate();
         this.year = this.currentTime.getFullYear();
         this.tasks = [
-            new Task("Finish weekend Angular homework"),
-            new Task('Begin brainstorming possible JavaScript group projects'),
-            new Task('Add README file to last few Angular repos on GitHub')
+            new Task("Finish weekend Angular homework", 3),
+            new Task('Begin brainstorming possible JavaScript group projects', 2),
+            new Task('Add README file to last few Angular repos on GitHub', 2)
         ];
     }
+    AppComponent.prototype.editTask = function () {
+        alert("you just edited!");
+    };
+    AppComponent.prototype.isDone = function (clickedTask) {
+        if (clickedTask.done === true) {
+            alert("Done!");
+        }
+        else {
+            alert("Not done slacker!");
+        }
+    };
+    AppComponent.prototype.priorityColor = function (currentTask) {
+        if (currentTask.priority === 3) {
+            return "bg-danger";
+        }
+        else if (currentTask.priority === 2) {
+            return "bg-warning";
+        }
+        else {
+            return "bg-info";
+        }
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'app-root',
-        template: "\n  <div class=\"container\">\n    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>\n    <h3>{{currentFocus}}</h3>\n    <ul>\n    <li *ngFor=\"let currentTask of tasks\">{{currentTask.description}}</li>\n    </ul>\n  </div>\n  "
+        template: "\n  <div class=\"container\">\n    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>\n    <h3>{{currentFocus}}</h3>\n    <ul>\n    <li [class]=\"priorityColor(currentTask)\" (click)=\"isDone(currentTask)\" *ngFor=\"let currentTask of tasks\">{{currentTask.description}}  <button (click)=\"editTask()\">Edit!</button></li>\n    </ul>\n  </div>\n  "
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
 var Task = (function () {
-    function Task(description) {
+    function Task(description, priority) {
         this.description = description;
+        this.priority = priority;
         this.done = false;
     }
     return Task;
--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-    <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+    <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
     </ul>
   </div>
   `
@@ -20,13 +20,36 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task("Finish weekend Angular homework"),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task("Finish weekend Angular homework", 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+
+
+  editTask() {
+    alert("you just edited!");
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("Done!");
+    } else {
+      alert("Not done slacker!");
+    }
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
 export class Task {
     public done: boolean = false;
-    constructor(public description: string) {}
+    constructor(public description: string, public priority: number) {}
 }
